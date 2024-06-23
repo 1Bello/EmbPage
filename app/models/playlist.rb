948815c1ef24d464d59565eacbe424db7725f484
Ignacio <ibello@miuandes.cl>
2024-06-23 16:43:31 -0400

    db clean

--- app/models/playlist.rb
+++
@@ -1,3 +0,0 @@
-class Playlist < ApplicationRecord
-    has_many :songs
-end
--- db/migrate/20240602201708_add_details_to_songs.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToSongs < ActiveRecord::Migration[7.1]
-  def change
-    add_column :songs, :artist_name, :string
-    add_column :songs, :song_image, :string
-  end
-end
