34a9093be86c0f7b1e5f3122b79cc4fa9ae1eeb7
lucasfreyess <lfreyes1@miuandes.cl>
2024-06-24 15:44:50 -0400

    ajax5.0 salvame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,20 @@ class ApplicationController < ActionController::Base
         @received_json = @@received_json
     end
 
+    #def receive_data
+        #@@received_json = request.raw_post
+        #render json: @@received_json
     def receive_data
-        @@received_json = request.raw_post
-        render json: @@received_json
+        @@received_json = request.body.read
+        render json: { message: JSON.parse(@@received_json) }
+    rescue JSON::ParserError => e
+        render json: { error: "Invalid JSON format: #{e.message}" }, status: 400
     end
 
     def fetch_data
-        render json: @@received_json
+        #render json: @@received_json
+
+        render json: { message: @@received_json }
     end
 
 end
