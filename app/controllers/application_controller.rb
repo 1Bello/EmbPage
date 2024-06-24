5226b08477a593b1ffd2aed4572718c715533dea
lucasfreyess <lfreyes1@miuandes.cl>
2024-06-24 13:28:59 -0400

    first test.. lacks ajax!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+    #protect_from_forgery with: :null_session
+    
+    @@received_json = nil
+    
+    def index
+        @received_json = @@received_json
+    end
+
+    def receive_data
+        @@received_json = request.raw_post
+        render json: @@received_json
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
   resources :playlists do
     resources :songs
   end
   resources :songs, only: [:show]
   get '/auth/spotify/callback', to: 'users#spotify'
   resources :home
+  
+  
   root "application#index"
+  post 'receive_data', to: 'application#receive_data'
 end
